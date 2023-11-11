21d4f87984b6ded76effb4c29ec4b8dad7c0a5c8
KBowler1 <122831413+KBowler1@users.noreply.github.com>
2023-11-11 17:18:12 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Jeopardy</h1>
+                {/* Add other components or content here */}
+            </header>
+        </div>
+    );
+}
+
+export default App;
